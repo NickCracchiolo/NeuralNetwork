a39529f7f4ffdab4b328a91b3a6e098cf7728c7e
Nick <nick@cracchiolo.com>
2017-01-22 14:45:02 -0800

    Initial Commit

---
+++ NeuralNetwork/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NeuralNetwork
+//
+//  Created by Nicholas Cracchiolo on 1/22/17.
+//  Copyright © 2017 Nicholas Cracchiolo. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
